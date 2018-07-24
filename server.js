31343686a2c415242f380869e8361ef428cf65eb
Eva Killenberg <evakill@seas.upenn.edu>
2018-07-24 09:56:23 -0700

    with comments

--- server.js
+++ server.js
@@ -1,42 +1,52 @@
 const express = require('express');
+const request = require('request')
 const bodyParser = require('body-parser')
 const path = require('path');
 const dialogflow = require('dialogflow');
+var http = require('https');
+var querystring = require('querystring');
+const { IncomingWebhook, WebClient, RTMClient } = require('@slack/client');
 
 const app = express();
 
-const projectId = process.env.DIALOGFLOW_PROJECT_ID;
-const sessionId = 'quickstart-session-id';
-const query = 'hello';
-const languageCode = 'en-US';
-
-const sessionClient = new dialogflow.SessionsClient();
-const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-const { IncomingWebhook, WebClient, RTMClient } = require('@slack/client');
+//clients setup
 const token = process.env.SLACK_TOKEN;
-//const bot = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
 const rtm = new RTMClient(token);
 const web = new WebClient(token);
-const channel = 'DBVG1LQR3';
+
+
+//dialogflow setup
+const projectId = process.env.DIALOGFLOW_PROJECT_ID;
+const sessionId = 'quickstart-session-id';
+const languageCode = 'en-US';
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+//const bot = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
 
 rtm.start();
+
+//need a way to find channel!!
+const channel = 'DBVG1LQR3';
 
 rtm.on('connected', (e) => {
   console.log('rtm connected to ', channel);
 
+  //connected event
   rtm.sendMessage('whaddup. it\'s me bish. let\'s schedule some shit.', channel)
   .then(res=>{
     console.log('Message sent')
   })
   .catch(console.error);
 
+  //message event
   rtm.on('message', (message) => {
-    // Skip messages that are from a bot or my own user ID
+    // skip messages that are from a bot or my own user ID
     if ( (message.subtype && message.subtype === 'bot_message') ||
          (!message.subtype && message.user === rtm.activeUserId) ) {
       return;
     }
+
+    //parse message
     var text = message.text.trim().toLowerCase();
     const request = {
       session: sessionPath,
@@ -47,6 +57,8 @@ rtm.on('connected', (e) => {
         },
       },
     };
+
+    //respond with dialogflow
     sessionClient
     .detectIntent(request)
     .then(responses => {
